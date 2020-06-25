8e7540301814ce3e69e2bddef448c473c1be3914
serenathai <z5159999@unsw.edu.au>
2020-06-25 12:27:38 +1000

    Hi I added a comment in the main activity

--- app/src/main/java/com/infs3605/infs3605/MainActivity.java
+++ app/src/main/java/com/infs3605/infs3605/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//hello im commenting in the main activity
 public class MainActivity extends AppCompatActivity {
 
     @Override
