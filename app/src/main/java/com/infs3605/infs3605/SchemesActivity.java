7e9972d1a679d8616dc1d686a3fa4d50b4e550f5
serenathai <z5159999@unsw.edu.au>
2020-06-26 15:54:00 +1000

    Added bottom naviation pane to main activity xml file. Currently not functioning, does not launch activities

--- app/src/main/java/com/infs3605/infs3605/SchemesActivity.java
+++ app/src/main/java/com/infs3605/infs3605/SchemesActivity.java
@@ -3,6 +3,7 @@ package com.infs3605.infs3605;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class SchemesActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class SchemesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schemes);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("https://www.business.gov.au/Grants-and-Programs");
+
     }
 }
