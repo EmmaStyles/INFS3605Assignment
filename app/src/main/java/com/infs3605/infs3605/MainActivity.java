01ea27b2bac75d53647fe41a6d4e3ae37cfbbe34
serenathai <z5159999@unsw.edu.au>
2020-07-02 13:12:49 +1000

    Added bottom navigation pane with four icons. Currently not functioning, does not launch activities

--- app/src/main/java/com/infs3605/infs3605/MainActivity.java
+++ app/src/main/java/com/infs3605/infs3605/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 //hello im commenting in the main activity
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
     }
 }
