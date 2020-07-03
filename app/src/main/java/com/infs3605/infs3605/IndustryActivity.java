9c5c9c72c5bc816fe83de0e6f86bd793945e03d0
audreypatricia <55723938+audreypatricia@users.noreply.github.com>
2020-07-03 12:40:23 +1000

    cleaned code for IndustryActivity

--- app/src/main/java/com/infs3605/infs3605/IndustryActivity.java
+++ app/src/main/java/com/infs3605/infs3605/IndustryActivity.java
@@ -22,8 +22,7 @@ public class IndustryActivity extends AppCompatActivity implements AdapterView.O
     private RecyclerView recyclerView;
     private IndustryActivityAdapter mIndustryActAdapter;
     private RecyclerView.LayoutManager layoutManager;
-//    ArrayList<Article> myDataset = new ArrayList<Article>();
-//    ArrayList<Article> getMyDatasetFiltered = new ArrayList<Article>(); //commented for camposha filter
+
 IndustryClass industryClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,21 +54,13 @@ IndustryClass industryClass;
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
-//        myDataset.add(new Article("Travel", "Hotels", "Accommodation, Holiday Homes & Holiday Rentals", "23/06/2020", "Accommodation services may be offered without restriction, except holiday rentals which are subject to restrictions.\n"));
-//        myDataset.add(new Article("Property", "Real Estate", "Auction houses (other than Clearing houses)", "23/06/2020", "Accommodation services may be offered without restriction, except holiday rentals which are subject to restrictions.\n"));
-//        myDataset.add(new Article("Hospitality", "Bars", "Bars, pubs, clubs, cellar doors, micro-breweries, distilleries and casinos", "23/06/2020", "Capacity must not exceed 50 customers or one customer per 4 square metres (excluding staff) per existing separate seated food or drink area.\n"));
-//        myDataset.add(new Article("Beauty", "Nail Salons", "Beauty, nail, tanning and waxing salons, tattoo and massage parlours and spas", "23/06/2020", "Capacity must not exceed 20 customers or one customer per 4 square metres.\n"));
-//        myDataset.add(new Article("Hospitality", "Restaurants", "Cafes and restaurants", "23/06/2020", "Capacity must not exceed 50 customers or one customer per 4 square metres.\n"));
-
-        // commented only for camposha tutorial addition, without it this sld be uncommented
-//        mIndustryActAdapter = new IndustryActivityAdapter(getMyDatasetFiltered);
         recyclerView.setLayoutManager(layoutManager);
-//        recyclerView.setAdapter(mIndustryActAdapter); commented for camposha tutorial
+
     }
     ArrayList<Article> data = new ArrayList<Article>();
-    //camposha tutorial
+
     private ArrayList<Article> getArticles(){
-//        ArrayList<Article> data = new ArrayList<Article>();
+
         data.clear();
 
         data.add(new Article("Travel", "Hotels", "Accommodation, Holiday HomeS & Holiday Rentals", "23/06/2020", "Accommodation services may be offered without restriction, except holiday rentals which are subject to restrictions.\n"));
@@ -93,12 +84,12 @@ IndustryClass industryClass;
                 if(data.get(i).getIndustry().equalsIgnoreCase(industryChosen)){
                     articlesToDisplay.add(new Article(data.get(i).getIndustry(), data.get(i).getSegment() ,data.get(i).getTitle(), data.get(i).getDate(), data.get(i).getContent()));
                 }else{
-//                    data.remove(i);
+
                 }
 
             }
             mIndustryActAdapter = new IndustryActivityAdapter(articlesToDisplay);
-//            mIndustryActAdapter = new IndustryActivityAdapter(getArticles());
+
 
         } else{
             articlesToDisplay.clear();
@@ -112,21 +103,15 @@ IndustryClass industryClass;
         recyclerView.setAdapter(mIndustryActAdapter);
 
     }
-    //camposha tutorial addition end
+
 String segmentChosen;
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
          segmentChosen = adapterView.getItemAtPosition(position).toString();
         Toast.makeText(adapterView.getContext(), segmentChosen, Toast.LENGTH_SHORT ).show();
 
-        getSelectedSegment(segmentChosen); //added for camposha tutorial
+        getSelectedSegment(segmentChosen);
 
-        //commented out for the camposha tutorial
-//        for(int i=0; i<myDataset.size(); i++){
-//            if (myDataset.get(i).segment.equalsIgnoreCase(segmentChosen)){
-//                getMyDatasetFiltered.add(new Article(myDataset.get(i).industry, myDataset.get(i).segment, myDataset.get(i).title, myDataset.get(i).date, myDataset.get(i).content));
-//            }
-//        }
     }
 
     @Override
